2c8fa03b4ce77b5deeecee598c5c7167f2b3efd6
unknown <dirisio.pablo@gmail.com>
2015-11-28 01:15:00 -0300

    srtbro

---
+++ src/AerolineaFrba/ABM/Abm Rol/AsignarFuncionalidades.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace AerolineaFrba.ABM.Abm_Rol
+{
+    partial class AsignarFuncionalidades
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.matriculaLabel = new System.Windows.Forms.Label();
+            this.rolTexto = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.devolucionCheck = new System.Windows.Forms.CheckBox();
+            this.generarViajeCheck = new System.Windows.Forms.CheckBox();
+            this.abmRutaCheck = new System.Windows.Forms.CheckBox();
+            this.abmAeronaveCheck = new System.Windows.Forms.CheckBox();
+            this.abmRolCheck = new System.Windows.Forms.CheckBox();
+            this.cancelarFuncionalidades = new System.Windows.Forms.Button();
+            this.confirmarFuncionalidades = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // matriculaLabel
+            // 
+            this.matriculaLabel.AutoSize = true;
+            this.matriculaLabel.Location = new System.Drawing.Point(6, 28);
+            this.matriculaLabel.Name = "matriculaLabel";
+            this.matriculaLabel.Size = new System.Drawing.Size(29, 13);
+            this.matriculaLabel.TabIndex = 0;
+            this.matriculaLabel.Text = "Rol: ";
+            // 
+            // rolTexto
+            // 
+            this.rolTexto.AutoSize = true;
+            this.rolTexto.Location = new System.Drawing.Point(41, 28);
+            this.rolTexto.Name = "rolTexto";
+            this.rolTexto.Size = new System.Drawing.Size(32, 13);
+            this.rolTexto.TabIndex = 3;
+            this.rolTexto.Text = "{text}";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.matriculaLabel);
+            this.groupBox1.Controls.Add(this.rolTexto);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(246, 69);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Rol Seleccionado";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.devolucionCheck);
+            this.groupBox2.Controls.Add(this.generarViajeCheck);
+            this.groupBox2.Controls.Add(this.abmRutaCheck);
+            this.groupBox2.Controls.Add(this.abmAeronaveCheck);
+            this.groupBox2.Controls.Add(this.abmRolCheck);
+            this.groupBox2.Location = new System.Drawing.Point(12, 87);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(246, 140);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Funcionalidades Disponibles";
+            // 
+            // devolucionCheck
+            // 
+            this.devolucionCheck.AutoSize = true;
+            this.devolucionCheck.Location = new System.Drawing.Point(9, 111);
+            this.devolucionCheck.Name = "devolucionCheck";
+            this.devolucionCheck.Size = new System.Drawing.Size(80, 17);
+            this.devolucionCheck.TabIndex = 18;
+            this.devolucionCheck.Text = "Devolución";
+            this.devolucionCheck.UseVisualStyleBackColor = true;
+            // 
+            // generarViajeCheck
+            // 
+            this.generarViajeCheck.AutoSize = true;
+            this.generarViajeCheck.Location = new System.Drawing.Point(9, 88);
+            this.generarViajeCheck.Name = "generarViajeCheck";
+            this.generarViajeCheck.Size = new System.Drawing.Size(90, 17);
+            this.generarViajeCheck.TabIndex = 17;
+            this.generarViajeCheck.Text = "Generar Viaje";
+            this.generarViajeCheck.UseVisualStyleBackColor = true;
+            // 
+            // abmRutaCheck
+            // 
+            this.abmRutaCheck.AutoSize = true;
+            this.abmRutaCheck.Location = new System.Drawing.Point(9, 65);
+            this.abmRutaCheck.Name = "abmRutaCheck";
+            this.abmRutaCheck.Size = new System.Drawing.Size(75, 17);
+            this.abmRutaCheck.TabIndex = 16;
+            this.abmRutaCheck.Text = "ABM Ruta";
+            this.abmRutaCheck.UseVisualStyleBackColor = true;
+            // 
+            // abmAeronaveCheck
+            // 
+            this.abmAeronaveCheck.AutoSize = true;
+            this.abmAeronaveCheck.Location = new System.Drawing.Point(9, 42);
+            this.abmAeronaveCheck.Name = "abmAeronaveCheck";
+            this.abmAeronaveCheck.Size = new System.Drawing.Size(98, 17);
+            this.abmAeronaveCheck.TabIndex = 15;
+            this.abmAeronaveCheck.Text = "ABM Aeronave";
+            this.abmAeronaveCheck.UseVisualStyleBackColor = true;
+            // 
+            // abmRolCheck
+            // 
+            this.abmRolCheck.AutoSize = true;
+            this.abmRolCheck.Location = new System.Drawing.Point(9, 19);
+            this.abmRolCheck.Name = "abmRolCheck";
+            this.abmRolCheck.Size = new System.Drawing.Size(68, 17);
+            this.abmRolCheck.TabIndex = 14;
+            this.abmRolCheck.Text = "ABM Rol";
+            this.abmRolCheck.UseVisualStyleBackColor = true;
+            // 
+            // cancelarFuncionalidades
+            // 
+            this.cancelarFuncionalidades.Location = new System.Drawing.Point(174, 245);
+            this.cancelarFuncionalidades.Name = "cancelarFuncionalidades";
+            this.cancelarFuncionalidades.Size = new System.Drawing.Size(84, 23);
+            this.cancelarFuncionalidades.TabIndex = 7;
+            this.cancelarFuncionalidades.Text = "Cancelar";
+            this.cancelarFuncionalidades.UseVisualStyleBackColor = true;
+            // 
+            // confirmarFuncionalidades
+            // 
+            this.confirmarFuncionalidades.Location = new System.Drawing.Point(12, 245);
+            this.confirmarFuncionalidades.Name = "confirmarFuncionalidades";
+            this.confirmarFuncionalidades.Size = new System.Drawing.Size(73, 23);
+            this.confirmarFuncionalidades.TabIndex = 6;
+            this.confirmarFuncionalidades.Text = "Confirmar";
+            this.confirmarFuncionalidades.UseVisualStyleBackColor = true;
+            this.confirmarFuncionalidades.Click += new System.EventHandler(this.confirmarFuncionalidades_Click);
+            // 
+            // AsignarFuncionalidades
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(270, 291);
+            this.Controls.Add(this.cancelarFuncionalidades);
+            this.Controls.Add(this.confirmarFuncionalidades);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "AsignarFuncionalidades";
+            this.Text = "Asignar Funcionalidades";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label matriculaLabel;
+        private System.Windows.Forms.Label rolTexto;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox devolucionCheck;
+        private System.Windows.Forms.CheckBox generarViajeCheck;
+        private System.Windows.Forms.CheckBox abmRutaCheck;
+        private System.Windows.Forms.CheckBox abmAeronaveCheck;
+        private System.Windows.Forms.CheckBox abmRolCheck;
+        private System.Windows.Forms.Button cancelarFuncionalidades;
+        private System.Windows.Forms.Button confirmarFuncionalidades;
+
+    }
+}
---
+++ src/AerolineaFrba/ABM/Abm Rol/AsignarFuncionalidades.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using AerolineaFrba.Enums;
+using AerolineaFrba.Models;
+using AerolineaFrba.Services;
+
+namespace AerolineaFrba.ABM.Abm_Rol
+{
+    public partial class AsignarFuncionalidades : Form
+    {
+        public Rol rol { get; set; }
+        public DialogResult dr { get; set; }
+
+        public AsignarFuncionalidades(Rol rol, List<Funcionalidad_Rol> funcionalidadesRol)
+        {
+            InitializeComponent();
+            this.rolTexto.Text = rol.Descripcion;
+            this.rol = rol;
+
+            foreach (var funcionalidadRol in funcionalidadesRol){
+                switch (funcionalidadRol.Funcionalidad.Descripcion){
+                    case "ABM_Rol":
+                        this.abmRolCheck.Checked = true;
+                        break;
+                    case "ABM_Aeronave":
+                        this.abmAeronaveCheck.Checked = true;
+                        break;
+                    case "ABM_Ruta":
+                        this.abmRutaCheck.Checked = true;
+                        break;
+                    case "Generar_Viaje":
+                        this.generarViajeCheck.Checked = true;
+                        break;
+                    case "Devolucion":
+                        this.devolucionCheck.Checked = true;
+                        break;
+                }
+                    
+            }
+
+        }
+
+        private void Guardar_Click(object sender, EventArgs e) {
+        }
+
+        private void Cancelar_Click(object sender, EventArgs e) {
+            this.Close();
+        }
+
+        private void confirmarFuncionalidades_Click(object sender, EventArgs e)
+        {
+            DAO.connect();
+            List<Funcionalidad_Rol> funcionalidadesRol = DAO.selectAll<Funcionalidad_Rol>(new[] { "rol_id = " + this.rol.Id });
+
+            foreach (var funcionalidadRol in funcionalidadesRol)
+            {
+                DAO.delete<Funcionalidad_Rol>(funcionalidadRol);
+            }
+
+            Funcionalidad_Rol fRol = new Funcionalidad_Rol();
+            fRol.Rol_Id = this.rol.Id;
+
+            if (this.abmRolCheck.Checked)
+            {
+                fRol.Funcionalidad_Id = 1; //TODO: MAKE ME A CONSTANT DOOD
+                DAO.insert<Funcionalidad_Rol>(fRol);
+            }
+
+            if (this.abmAeronaveCheck.Checked)
+            {
+                fRol.Funcionalidad_Id = 2; //TODO: MAKE ME A CONSTANT DOOD
+                DAO.insert<Funcionalidad_Rol>(fRol);
+            }
+
+            if (this.abmRutaCheck.Checked)
+            {
+                fRol.Funcionalidad_Id = 3; //TODO: MAKE ME A CONSTANT DOOD
+                DAO.insert<Funcionalidad_Rol>(fRol);
+            }
+
+            if (this.generarViajeCheck.Checked)
+            {
+                fRol.Funcionalidad_Id = 4; //TODO: MAKE ME A CONSTANT DOOD
+                DAO.insert<Funcionalidad_Rol>(fRol);
+            }
+
+            if (this.devolucionCheck.Checked)
+            {
+                fRol.Funcionalidad_Id = 5; //TODO: MAKE ME A CONSTANT DOOD
+                DAO.insert<Funcionalidad_Rol>(fRol);
+            }
+
+            DAO.closeConnection();
+            this.dr = DialogResult.OK;
+            this.Close();
+
+        }
+
+    }
+}
