6a931dbdc09579ed8917c1a140b2ec3fe745604b
leopaglia <paglialungalf@gmail.com>
2015-12-06 05:47:59 -0300

    4 de 5 estadisticas

--- src/AerolineaFrba/Forms/Listado Estadistico/ListadoEstadistico.cs
+++ src/AerolineaFrba/Forms/Listado Estadistico/ListadoEstadistico.cs
@@ -36,23 +36,25 @@ namespace AerolineaFrba.Forms.Listado_Estadistico {
         }
 
         private void destinosPasajesButton_Click(object sender, EventArgs e) {
+
             String title = "Destinos más comprados";
 
             String rango = this.getRango();
 
-            String tabla = Viaje.TableName + " v";
-            String pasajeTabla = Pasaje.TableName + " p";
-            String rutaTabla = Ruta.TableName + " r";
-            String ciudadTabla = Ciudad.TableName + " c";
-
             String query = "";
-            query += "SELECT TOP 5 c.descripcion as 'Destino', count(1) as 'Ventas'" + " FROM " + tabla;
-            query += " INNER JOIN " + pasajeTabla + " ON p.viaje_id = v.id";
-            query += " INNER JOIN " + rutaTabla + " ON v.ruta_id = r.id";
-            query += " INNER JOIN " + ciudadTabla + " ON r.ciudad_destino_id = c.id";
-            query += " WHERE (p.fecha_compra " + rango + ")";
-            query += " GROUP BY c.descripcion";
-            query += " ORDER BY 'Ventas' DESC";
+
+            query += "select top 5 d.descripcion as 'Destino', count(1) as 'Ventas'";
+            query += " from BIEN_MIGRADO_RAFA.Compra_Pasaje";
+            query += " inner join BIEN_MIGRADO_RAFA.Pasaje p on pasaje_id = p.id";
+            query += " inner join BIEN_MIGRADO_RAFA.Viaje v on p.viaje_id = v.id";
+            query += " inner join BIEN_MIGRADO_RAFA.Ruta r on v.ruta_id = r.id";
+            query += " inner join BIEN_MIGRADO_RAFA.Ciudad d on r.ciudad_destino_id = d.id";
+            query += " inner join BIEN_MIGRADO_RAFA.Compra c on compra_id = c.id";
+
+            query += " WHERE (c.fecha_compra " + rango + ")";
+
+            query += " group by d.descripcion";
+            query += " order by count(1) DESC"; 
 
             DataTable table = this.buildDatatable(query);
 
@@ -65,27 +67,81 @@ namespace AerolineaFrba.Forms.Listado_Estadistico {
 
         private void destinosVaciosButton_Click(object sender, EventArgs e) {
 
+            String title = "Destinos más comprados";
+
+            String rango = this.getRango();
+
+            String query = "";
+            //TODO hay que arreglar la query, pero viene por aca
+            /*
+             
+                select top 5
+                d.descripcion, 
+                v.aeronave_id, 
+                v.id as 'viaje id',
+                (select count(1) from BIEN_MIGRADO_RAFA.Butaca b where b.aeronave_id = v.aeronave_id) - (select count(1) from BIEN_MIGRADO_RAFA.Pasaje where viaje_id = v.id) as 'butacas libres' 
+
+                from BIEN_MIGRADO_RAFA.Compra_Pasaje cp
+
+                inner join BIEN_MIGRADO_RAFA.Pasaje p on cp.pasaje_id = p.id 
+                inner join BIEN_MIGRADO_RAFA.Viaje v on p.viaje_id = v.id 
+                inner join BIEN_MIGRADO_RAFA.Ruta r on v.ruta_id = r.id 
+                inner join BIEN_MIGRADO_RAFA.Ciudad d on r.ciudad_destino_id = d.id 
+                inner join BIEN_MIGRADO_RAFA.Compra c on cp.compra_id = c.id 
+
+                WHERE (c.fecha_compra BETWEEN '2015-07-01' AND '2015-12-31') 
+
+                order by [butacas libres] DESC 
+              
+             
+             */
+
+            DataTable table = this.buildDatatable(query);
+
+            TableDialog dialog = new TableDialog(table, title);
+
+            dialog.ShowDialog();
+
         }
 
-        private void clientesPuntosButton_Click(object sender, EventArgs e) { //FIXME fechas
+        private void clientesPuntosButton_Click(object sender, EventArgs e) {
 
             String title = "Clientes con más puntos acumulados";
             
             String rango = this.getRango();
 
-            String tabla = Cliente.TableName + " c";
-            String pasajeTabla = Pasaje.TableName + " p";
-            String paqueteTabla = Paquete.TableName + " pq";
 
-            String puntos = "( SELECT ( CAST (SUM(p.precio) + SUM(pq.precio) AS INT) / 10 ) ) AS 'puntos acumulados'";
+            String queryPasajes = "";
+
+            queryPasajes += "(select sum(precio) from BIEN_MIGRADO_RAFA.Pasaje p ";
+            queryPasajes += "inner join BIEN_MIGRADO_RAFA.Compra c on c.cliente_id = cl.id ";
+            queryPasajes += "inner join BIEN_MIGRADO_RAFA.Compra_Pasaje cp on c.id = cp.compra_id and cp.pasaje_id = p.id ";
+            queryPasajes += " WHERE (c.fecha_compra " + rango + ")";
+            queryPasajes += ") ";
+
+            String queryPaquetes = "";
+
+            queryPaquetes += "(select sum(precio) from BIEN_MIGRADO_RAFA.Paquete p ";
+            queryPaquetes += "inner join BIEN_MIGRADO_RAFA.Compra c on c.cliente_id = cl.id ";
+            queryPaquetes += "inner join BIEN_MIGRADO_RAFA.Compra_Paquete cp on c.id = cp.compra_id and cp.paquete_id = p.id ";
+            queryPaquetes += " WHERE (c.fecha_compra " + rango + ")";
+            queryPaquetes += ") ";
+
+            String queryCanjes = "";
+
+            queryCanjes += "(select sum(c.cantidad * cat.costo) from BIEN_MIGRADO_RAFA.Canje c ";
+            queryCanjes += "inner join BIEN_MIGRADO_RAFA.Catalogo cat on c.catalogo_id = cat.id ";
+            queryCanjes += "where c.cliente_id = cl.id ";
+            queryCanjes += "and (c.fecha " + rango + ") ";
+            queryCanjes += ") ";
+
+            String queryPuntos = queryPasajes + " + " + queryPaquetes + " - " + queryCanjes;  
 
             String query = "";
-            query += "SELECT TOP 5 c.id, " + puntos + " FROM " + tabla;
-            query += " INNER JOIN " + pasajeTabla + " ON p.cliente_id = c.id";
-            query += " INNER JOIN " + paqueteTabla + " ON pq.cliente_id = c.id";
-            query += " WHERE (p.fecha_compra " + rango + ") AND (pq.fecha_compra " + rango + ")";
-            query += " GROUP BY c.id, p.cliente_id, pq.cliente_id";
-            query += " ORDER BY 'puntos acumulados' DESC";
+
+            query += "select top 5 cl.nombre + ' ' + cl.apellido as 'Cliente', " + queryPuntos + " as 'Puntos Acumulados' ";
+            query += " from BIEN_MIGRADO_RAFA.Cliente cl";
+            query += " order by [Puntos Acumulados] DESC"; 
 
             DataTable table = this.buildDatatable(query);
 
@@ -97,9 +153,56 @@ namespace AerolineaFrba.Forms.Listado_Estadistico {
 
         private void destinosCanceladosButton_Click(object sender, EventArgs e) {
 
+            String title = "Destinos más cancelados";
+
+            String rango = this.getRango();
+
+            String query = "";
+
+            query += "select top 5 d.descripcion as 'Destino', count(1) as 'Cancelaciones'";
+            query += " from BIEN_MIGRADO_RAFA.Cancelacion_Pasaje";
+            query += " inner join BIEN_MIGRADO_RAFA.Pasaje p on pasaje_id = p.id";
+            query += " inner join BIEN_MIGRADO_RAFA.Viaje v on p.viaje_id = v.id";
+            query += " inner join BIEN_MIGRADO_RAFA.Ruta r on v.ruta_id = r.id";
+            query += " inner join BIEN_MIGRADO_RAFA.Ciudad d on r.ciudad_destino_id = d.id";
+            query += " inner join BIEN_MIGRADO_RAFA.Cancelacion c on cancelacion_id = c.id";
+
+            query += " WHERE (c.fecha " + rango + ")";
+
+            query += " group by d.descripcion";
+            query += " order by count(1) DESC";
+
+            DataTable table = this.buildDatatable(query);
+
+            TableDialog dialog = new TableDialog(table, title);
+
+            dialog.ShowDialog();
+
         }
 
         private void aeronaveBajaButton_Click(object sender, EventArgs e) {
+
+            String title = "Aeronaves con más días fuera de servicio";
+
+            String rango = this.getRango();
+
+            String query = "";
+
+            query += "select top 5 a.matricula as 'Matrícula', sum(DATEDIFF(DAY, b.fecha_baja, b.fecha_reinicio)) as 'Días fuera de servicio'";
+            query += " from BIEN_MIGRADO_RAFA.Aeronave a";
+            query += " inner join BIEN_MIGRADO_RAFA.Baja_Aeronave b on a.id = b.aeronave_id";
+
+            query += " WHERE (b.fecha_baja " + rango + ") AND b.tipo_baja_id != 2";
+
+            query += " group by a.matricula";
+            query += " order by count(1) DESC";
+
+
+            DataTable table = this.buildDatatable(query);
+
+            TableDialog dialog = new TableDialog(table, title);
+
+            dialog.ShowDialog();
 
         }
 
@@ -123,15 +226,14 @@ namespace AerolineaFrba.Forms.Listado_Estadistico {
 
             var dataSource = new List<Anio>();
 
-            //dataSource.Add(new Anio() { Value = 2010 });
-            //dataSource.Add(new Anio() { Value = 2011 });
-            //dataSource.Add(new Anio() { Value = 2012 });
-            //dataSource.Add(new Anio() { Value = 2013 });
-            dataSource.Add(new Anio() { Value = 2014 });
-            dataSource.Add(new Anio() { Value = 2015 });
-            dataSource.Add(new Anio() { Value = 2016 });
-            dataSource.Add(new Anio() { Value = 2017 });
+            int year = Config.SystemConfig.systemDate.Year;
 
+            int from = year - 3;
+            int to = year + 3;
+
+            for (int i = from; i <= to; i++)
+                dataSource.Add(new Anio() { Value = i });
+        
             this.anioCombo.DataSource = dataSource;
             this.anioCombo.DisplayMember = "Value";
             this.anioCombo.ValueMember = "Value";
@@ -144,7 +246,7 @@ namespace AerolineaFrba.Forms.Listado_Estadistico {
             String anio = this.anioCombo.SelectedValue.ToString();
 
             if (this.semestreCombo.SelectedIndex == 0) {
-                rango = "BETWEEN '" + anio + "-01-01'" + " AND '" + anio + "-06-31'";
+                rango = "BETWEEN '" + anio + "-01-01'" + " AND '" + anio + "-06-30'";
             }
             else {
                 rango = "BETWEEN '" + anio + "-07-01'" + " AND '" + anio + "-12-31'";
